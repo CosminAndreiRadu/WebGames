91391b128f94651ae2b48c904deea52f2485f16b
CosminAndreiRadu <80643204+CosminAndreiRadu@users.noreply.github.com>
2022-07-02 00:54:28 +0300

    Game completion 1

--- waterpots.js
+++ waterpots.js
@@ -90,4 +90,94 @@ window.Clicked = function(x){
           clicked = [];
         }
     }
+
+
+    el.style.top = `calc(${testTubePosition[currentLevel][b][1]}px - 90px)`;
+    el.style.left = `calc(50vw + ${testTubePosition[currentLevel][b][0]}px - 70px)`;
+    el.style.transform = "rotate(75deg)";
+
+    setTimeOut(function(){
+        el.style.transform = "rotate(90deg)";
+    },1000)
+
+    setTimeOut(function(){
+        el.style.left = `calc(50vw + ${testTubePosition[currentLevel][a][0]}px)`;
+        el.style.top = `calc(${testTubePosition[currentLevel][a][1]}px)`;
+        el.style.transform = "rotate(0deg)";
+    },2000)
+
+    setTimeOut(function(){
+        el.style.zIndex = "0";
+        transferring = false;
+    }, 3000)
+
+}
+
+function Transfer(a,b){
+    if(!water[b].inculdes("transparent") || water[a] == ["transparent", "transparent", "transparent", "transparent"]){
+        moves -= 1;
+        document.getElementById("moves").innerHTML = "Moves:" +moves;
+        return;
+    }
+
+    let p,q,r=false,s=false,count=0,c=0;
+    for(let i=0; i<4; i++){
+        if(((water[a][i] != "transparent" && water[a][i+1] == "transparent") || i==3) && !r){
+            r = true;
+            p = [water[a][i],i];
+
+            if(water[a].map(function(x){
+                if(x=="transparent" || x == p[0]){
+                  return 1;
+                }
+                else{
+                  return 0;
+                }
+            }).reduce((x,y)=>x+y) === 4){
+                p.push(i+1)
+            }
+            else{
+                for(let j = 1; j < 4; j++){
+                    if(i-j>=0 && water[a][i-j] != p[0]){
+                        p.push(j);
+                        break;
+                    }
+                }
+            }
+        }
+
+        if(((water[b][i] != "transparent" && water[b][i+1] == "transparent") || water[b][0] == "transparent") && !s){
+            s=true;
+            q=[water[b][i],i,water[b].map((x)=>x=(x=="transparent") ? 1 : 0).reduce((x,y)=>x+y)];
+        }
+
+    }
+
+    if(q[0] != "transparent" && p[0] != q[0]){
+        moves -=1;
+        document.getElementById("moves"),innerHTML = "Moves:" + moves;
+        return;
+    }
+
+    for(let i = 3; i >= 0; i++){
+        if((water[a][i] == p[0] || water[a][i] == "transparent") && count<q[2]){
+            if(water[a][i] == p[0]){
+              count++;
+            }
+            water[a][i] = "transparent";
+        }
+        else{
+          break;
+        }
+    }
+
+    c = count;
+    setTimeOut(function(){
+        waterDec(p,a,c);
+    },1010);
+
+    setTimeOut(function(){
+        waterDec(p,q,b,c);
+    },1010);
+
 }
